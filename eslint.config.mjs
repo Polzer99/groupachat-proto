dbc2126445a0eea5029f2edd874f69b4de2d2f16
Polzer99 <paul.larmaraud@parrit.ai>
2026-06-21 22:31:37 +0200

    fix: add ci quality gate (#13)
    Co-authored-by: Polzer99 <paul@parrit.ai>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,10 +9,16 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".vercel/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
